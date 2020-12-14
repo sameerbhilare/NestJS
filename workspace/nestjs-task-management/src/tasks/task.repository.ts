e5a5b3f7a231fc83887118edc5b5dfe67ef3a556
Sameer Bhilare <bhilare.sameer@gmail.com>
2020-12-14 14:25:20 +0530

    Creating Task in DB: using Repository

--- workspace/nestjs-task-management/src/tasks/task.repository.ts
+++ workspace/nestjs-task-management/src/tasks/task.repository.ts
@@ -1,8 +1,31 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 /*
     This tells the TypeORM that this repository is a repository for Tasks.
 */
 @EntityRepository(Task)
-export class TaskRepository extends Repository<Task> {}
+export class TaskRepository extends Repository<Task> {
+  // Create Task in DB
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    // DS6 destructuring
+    /*
+        If you have an object that contains certain key value pairs,
+        you can use this syntax to only extract the keys you care about
+        and they will be available within the current scope.
+    */
+    const { title, description } = createTaskDto;
+    // creating the task
+    const task = new Task();
+    task.title = title;
+    task.description = description;
+    task.status = TaskStatus.OPEN;
+
+    // saving the entity into database
+    await task.save();
+
+    return task;
+  }
+}

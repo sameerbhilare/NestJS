bd5f864ff6e67562d80adf721635f0d4371cb4c8
Sameer Bhilare <bhilare.sameer@gmail.com>
2020-12-14 16:20:11 +0530

    Signup validations: min, max length and strong password

--- workspace/nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
+++ workspace/nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  // at least one uppercase letter, at least one lowercase letter, at least one number or special character
+  //   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+  //     message: 'Password too weak.',
+  //   })
   password: string;
 }

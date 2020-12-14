2ff1366f5476006237450426cb696c516ff965a8
Sameer Bhilare <bhilare.sameer@gmail.com>
2020-12-14 10:27:58 +0530

    Error handling: getting a non existing task

--- workspace/nestjs-task-management/src/tasks/tasks.service.ts
+++ workspace/nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -36,7 +36,18 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    /* 
+      NestJS provides us with a set of exceptions we can throw that represent different HTTP error codes.
+      Since this is not caught in service or in controller, it will be sent back to the client.
+      But NestJS behind the scenes will beautifully format it and then send it back to the client.
+    */
+    if (!found) {
+      throw new NotFoundException(`Task with ID ${id} not found!`);
+    }
+
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {

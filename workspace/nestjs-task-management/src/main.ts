637246693d3eeb53fa93dab761468ecb4f5bde06
Sameer Bhilare <bhilare.sameer@gmail.com>
2020-12-15 18:07:54 +0530

    Passing Node Env for development

--- workspace/nestjs-task-management/src/main.ts
+++ workspace/nestjs-task-management/src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const serverConfig = config.get('server');
   const logger = new Logger('bootstrap'); // 'bootstrap' is just context for this logger since we are in bootstrap() function
   const app = await NestFactory.create(AppModule);
+  // enable CORS (Cross Origin Request Sharing)
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
